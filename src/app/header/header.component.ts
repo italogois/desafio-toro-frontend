3b610093dcc84f19b01b7d5a15fdcb8868e2a179
Italo Gois <italo.alberto26@gmail.com>
2025-03-26 09:48:06 -0300

    feat: first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent {}
