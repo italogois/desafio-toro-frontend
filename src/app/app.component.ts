233f40ea9f8e46e82c18846d3950c81215233bb3
Italo Gois <italo.alberto26@gmail.com>
2025-03-26 23:23:44 -0300

    feat: create component quote item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-
+import { QuoteItemComponent } from './quote-item/quote-item.component';
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, QuoteItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/quote-item/quote-item.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-quote-item',
+  imports: [CommonModule],
+  templateUrl: './quote-item.component.html',
+  styleUrl: './quote-item.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class QuoteItemComponent {}
